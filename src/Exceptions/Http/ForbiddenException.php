9432517da0c8c4ee0a648a1f098bb86d1473728f
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-07-20 06:40:15 +0200

    feat(exceptions): add more specific Http exception classes

---
+++ src/Exceptions/Http/ForbiddenException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Assegai\Core\Exceptions\Http;
+
+use Assegai\Core\Http\HttpStatus;
+use stdClass;
+
+class ForbiddenException extends HttpException
+{
+  public function __construct(array|string|stdClass $message = '')
+  {
+    parent::__construct($message);
+    $this->setStatus(HttpStatus::Forbidden());
+  }
+}
