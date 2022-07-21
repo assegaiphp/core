a2b03479bbcbe2f7eca9c0398c684a63d818dec6
Andrew Masiye <amasiye313@gmail.com>
2022-07-21 23:12:47 +0200

    feat(attributes): add missing request mapping attributes

--- src/Attributes/Post.php
+++ src/Attributes/Post.php
@@ -5,16 +5,11 @@ namespace Assegai\Core\Attributes;
 use Attribute;
 
 /**
- * The HTTP POST method sends data to the server. The type of the body of
- * the request is indicated by the Content-Type header.
+ * Route handler (method) Decorator. Routes HTTP POST requests to the specified path.
  *
- * The difference between PUT and POST is that PUT is idempotent: calling
- * it once or several times successively has the same effect (that is no
- * side effect), where successive identical POST may have additional
- * effects, like passing an order several times.
- *
- * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST
+ * @see [Routing](https://docs.assegaiphp.com/controllers#routing)
  */
+
 #[Attribute(Attribute::TARGET_METHOD|Attribute::TARGET_FUNCTION)]
 class Post
 {
