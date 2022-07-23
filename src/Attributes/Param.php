276be497be33490c5b5e8b506b846cb23867d4af
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-07-23 20:57:16 +0200

    refactor(attributes): add pipe support to Param attributes

--- src/Attributes/Param.php
+++ src/Attributes/Param.php
@@ -3,6 +3,7 @@
 namespace Assegai\Core\Attributes;
 
 use Assegai\Core\Http\Request;
+use Assegai\Core\Interfaces\IPipeTransform;
 use Attribute;
 use stdClass;
 
@@ -11,11 +12,37 @@ class Param
 {
   public readonly string|stdClass $value;
 
-  public function __construct(public readonly ?string $key = null)
+  /**
+   * @param string|null $key
+   * @param array|IPipeTransform|null $pipes
+   */
+  public function __construct(
+    public readonly ?string $key = null,
+    public readonly null|array|IPipeTransform $pipes = null
+  )
   {
     $request = Request::getInstance();
     $params = $request->getParams();
+    $value = ( !empty($this->key) ) ? ($params[$this->key] ?? $params) : json_decode(json_encode($params));
 
-    $this->value = ( !empty($this->key) ) ? ($params[$this->key] ?? $params) : json_decode(json_encode($params));
+    if ($this->pipes)
+    {
+      if (is_array($this->pipes))
+      {
+        foreach ($this->pipes as $pipe)
+        {
+          if ($pipe instanceof IPipeTransform)
+          {
+            $value = $pipe->transform($value);
+          }
+        }
+      }
+      else
+      {
+        $value = $this->pipes->transform($value);
+      }
+    }
+
+    $this->value = $value;
   }
 }
