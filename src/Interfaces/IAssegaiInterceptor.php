520dc321ad93a4d39fd4acfa77ddbe439e825675
Andrew Masiye <amasiye313@gmail.com>
2022-07-29 22:24:07 +0200

    feat(interfaces): create IAssegaiInterceptor.php

---
+++ src/Interfaces/IAssegaiInterceptor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Assegai\Core\Interfaces;
+
+use Assegai\Core\ExecutionContext;
+
+/**
+ * Interface describing implementation of an interceptor.
+ *
+ * @see [Interceptors](https://docs.assegaiphp.com/interceptors)
+ *
+ */
+interface IAssegaiInterceptor
+{
+  public function intercept(ExecutionContext $context): ExecutionContext;
+}
