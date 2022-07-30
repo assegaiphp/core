769eadc3e256d77aaabfe8ae4fb51ce2aecd5b99
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-07-30 07:31:18 +0200

    feat(interceptors): create FileInterceptorOptions.php

---
+++ src/Interceptors/FileInterceptorOptions.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Assegai\Core\Interceptors;
+
+use PhpParser\Node\Expr\Closure;
+
+class FileInterceptorOptions
+{
+  public function __construct(
+    public readonly string $dest = DEFAULT_STORAGE_PATH,
+    public readonly string $storage = DEFAULT_STORAGE_PATH,
+    public readonly ?Closure $fileFilter = null,
+    public readonly array $limits = [],
+    public readonly bool $preservePath = false
+  )
+  {
+  }
+}
