69fe2a6f5d7f058462b5eb786d5d8044a226b2bf
Andrew Masiye <amasiye313@gmail.com>
2022-07-30 07:31:18 +0200

    feat(interceptors): create FileInterceptorOptions.php

--- src/Interceptors/FileInterceptor.php
+++ src/Interceptors/FileInterceptor.php
@@ -4,18 +4,34 @@ namespace Assegai\Core\Interceptors;
 
 use Assegai\Core\Attributes\Injectable;
 use Assegai\Core\ExecutionContext;
+use Assegai\Core\Http\Requests\Request;
 use Assegai\Core\Interfaces\IAssegaiInterceptor;
 
 #[Injectable]
 class FileInterceptor implements IAssegaiInterceptor
 {
-  public function __construct(public readonly string $name)
+  public function __construct(
+    public readonly string $fieldName,
+    public readonly ?FileInterceptorOptions $options = null
+  )
   {
   }
 
-  public function intercept(ExecutionContext $context): ?callable
+  public function intercept(ExecutionContext $context, ?FileInterceptorOptions $options = null): ?callable
   {
     // TODO: Implement intercept() method.
+    if (!$options)
+    {
+      $options = new FileInterceptorOptions();
+    }
+
+    $requestBody = Request::getInstance()->getBody();
+    $key = $this->fieldName;
+    $file = $requestBody->$key;
+    $file['target_path'] = $options->dest . '/' . $file['name'];
+
+    Request::getInstance()->setFile($file);
+
     return null;
   }
 }
