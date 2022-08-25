f306f7d3734e1a720ef5a8999a3d4756d473d207
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-08-25 02:52:21 +0200

    feat(traits): create ViewRenderer.php

---
+++ src/Rendering/ViewMeta.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Assegai\Core\Rendering;
+
+/**
+ *
+ */
+class ViewMeta
+{
+  /**
+   *
+   */
+  public function __construct(
+    public readonly array $httpEquiv = [],
+    public readonly array $props = [],
+  )
+  {
+  }
+
+  /**
+   * @return string
+   */
+  public function __toString(): string
+  {
+    $html = $this->generateCharsetTag();
+    $html .= $this->generateHttpEquivTags();
+    $html .= $this->generateAttributeTags();
+
+    return $html;
+  }
+
+  /**
+   * @param array $meta
+   * @return static
+   */
+  public static function fromArray(array $meta): self
+  {
+    return new self(httpEquiv: $meta['httpEquiv'] ?? [], props: $meta['props'] ?? []);
+  }
+
+  /**
+   * @return string
+   */
+  private function generateCharsetTag(): string
+  {
+    return "<meta charset='UTF-8' />" . PHP_EOL;
+  }
+
+  /**
+   * @return string
+   */
+  private function generateHttpEquivTags(): string
+  {
+    $html = '';
+    foreach ($this->httpEquiv as $name => $content)
+    {
+      $html .= "<meta http-equiv='$name' content='$content' />" . PHP_EOL;
+    }
+    return $html;
+  }
+
+  /**
+   * @return string
+   */
+  private function generateAttributeTags(): string
+  {    $html = '';
+    foreach ($this->props as $name => $content)
+    {
+      $html .= "<meta name='$name' content='$content'>" . PHP_EOL;
+    }
+    return $html;
+  }
+}
