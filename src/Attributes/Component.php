497ea2958c22346a686b6e0ec9dc178d258f1bee
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-08-31 03:36:08 +0200

    docs(attribute): added PHPDoc block comments for all attributes.

--- src/Attributes/Component.php
+++ src/Attributes/Component.php
@@ -5,8 +5,8 @@ namespace Assegai\Core\Attributes;
 use Attribute;
 
 /**
- * An attribute that marks a class as an AssegaiPHP component and provides configuration metadata that determines how the
- * component should be processed, instantiated and used at runtime.
+ * An attribute that marks a class as an AssegaiPHP component and provides configuration metadata that determines
+ * how the component should be processed, instantiated and used at runtime.
  */
 #[Attribute(Attribute::TARGET_CLASS)]
 class Component
