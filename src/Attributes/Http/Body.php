2ecca81f8dcb8ed6bb5d8f1a444d700107e2737d
Andrew Masiye <andrew.masiye@liferaftinc.com>
2022-11-27 18:19:10 +0200

    feat(attributes/http): add pipe transformations to Body attribute

--- src/Attributes/Http/Body.php
+++ src/Attributes/Http/Body.php
@@ -3,7 +3,10 @@
 namespace Assegai\Core\Attributes\Http;
 
 use Assegai\Core\Attributes\Injectable;
+use Assegai\Core\Exceptions\Container\EntryNotFoundException;
 use Assegai\Core\Http\Requests\Request;
+use Assegai\Core\Http\Responses\Responder;
+use Assegai\Core\Interfaces\IPipeTransform;
 use Attribute;
 use stdClass;
 
@@ -16,10 +19,45 @@ class Body
   /**
    * @param string|null $key
    */
-  public function __construct(public readonly ?string $key = null)
+  public function __construct(
+    public readonly ?string $key = null,
+    public readonly array|IPipeTransform|string|null $pipes = null
+  )
   {
     $request = Request::getInstance();
-    $this->value = $request->getBody();
+    $value = $request->getBody();
+
+    if ($this->pipes)
+    {
+      if(is_string($this->pipes))
+      {
+        if (!is_subclass_of($this->pipes, IPipeTransform::class, true))
+        {
+          Responder::getInstance()->respond(new EntryNotFoundException($this->pipes));
+        }
+
+        /** @var IPipeTransform $pipe */
+        $pipe = new $this->pipes;
+        $value = $pipe->transform($value);
+      }
+      else if (is_array($this->pipes))
+      {
+        foreach ($this->pipes as $pipe)
+        {
+          if ($pipe instanceof IPipeTransform)
+          {
+            $value = $pipe->transform($value);
+          }
+        }
+      }
+      else
+      {
+        $value = $this->pipes->transform($value);
+      }
+    }
+
+
+    $this->value = $value;
 
     if (!empty($this->key) )
     {
