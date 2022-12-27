eee7039844d526e9f79769d1f0846210ae85d8e2
Andrew Masiye <amasiye313@gmail.com>
2022-12-27 13:26:30 +0200

    refactor(app): add useGlobalPipes method

--- src/Interfaces/IPipeTransform.php
+++ src/Interfaces/IPipeTransform.php
@@ -6,5 +6,10 @@ use stdClass;
 
 interface IPipeTransform
 {
+  /**
+   * @param mixed $value
+   * @param array|stdClass|null $metaData
+   * @return mixed
+   */
   public function transform(mixed $value, null|array|stdClass $metaData = null): mixed;
 }
