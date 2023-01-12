901ed47a0015aac4314b24e923618061a7ac8c27
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-01-12 13:42:07 +0200

    refactor(rendering): update ViewEngine.php
    - added default viewport meta attribute

--- src/Rendering/ViewEngine.php
+++ src/Rendering/ViewEngine.php
@@ -76,6 +76,9 @@ final class ViewEngine
 <!DOCTYPE html>
 <html lang="$lang">
   <head>
+    <meta name="viewport">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {$this->view->props}
   </head>
   <body>\n
