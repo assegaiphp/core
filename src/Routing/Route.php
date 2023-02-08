4dc2f68a4685cf1fe80ceb9cda0457b85111a153
Andrew Masiye <amasiye313@gmail.com>
2023-02-08 05:51:01 +0200

    refactor(routing): update Route.php
    - changed class to readonly class

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -4,11 +4,24 @@ namespace Assegai\Core\Routing;
 
 use Assegai\Core\Enumerations\Http\RequestMethod;
 
-class Route
+/**
+ * Class Route
+ *
+ * This class represents a Route in the AssegaiPHP framework.
+ *
+ * A route is defined by a path and a request method (GET, POST, etc.).
+ */
+readonly class Route
 {
+  /**
+   * Route constructor.
+   *
+   * @param string $path The path of the route.
+   * @param RequestMethod $method The request method for the route. Defaults to GET.
+   */
   public function __construct(
-    public readonly string $path,
-    public readonly RequestMethod $method = RequestMethod::GET
+    public string        $path,
+    public RequestMethod $method = RequestMethod::GET
   )
   {
   }
