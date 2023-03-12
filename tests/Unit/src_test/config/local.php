215a8b15bb729e7f68a97cbdeb8fea39023f0323
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-03-13 00:31:06 +0200

    test: create files for testing configuration

---
+++ tests/Unit/src_test/config/local.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+  'databases' => [
+    'mysql' => [
+      'assegai_test_db' => [
+        'host' => 'localhost',
+        'user' => 'shaka',
+        'password' => 'zulu',
+        'port' => 3306,
+      ]
+    ]
+  ]
+];
