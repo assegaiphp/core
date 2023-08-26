800bc3be1a8ae99d9e5453b2fd8935a8ff880467
Andrew Masiye <amasiye313@gmail.com>
2023-08-26 03:42:34 +0200

    docs: add documenting comments to ApiResponse.php

--- src/Http/Responses/ApiResponse.php
+++ src/Http/Responses/ApiResponse.php
@@ -5,10 +5,25 @@ namespace Assegai\Core\Http\Responses;
 use Assegai\Core\Http\Requests\Request;
 use JetBrains\PhpStorm\ArrayShape;
 
+/**
+ * ApiResponse class. This class is used to return data from a controller.
+ * It is used by the framework to return data to the client.
+ * @package Assegai\Core\Http\Responses
+ */
 class ApiResponse
 {
+  /**
+   * @var Request The request object.
+   */
   protected Request $request;
 
+  /**
+   * ApiResponse constructor.
+   *
+   * @param mixed $data The data to return.
+   * @param int|null $total The total number of records.
+   * @param Request|null $request The request object.
+   */
   public function __construct(
     public readonly mixed $data,
     public readonly ?int $total = null,
@@ -18,6 +33,11 @@ class ApiResponse
     $this->request = $request ?? Request::getInstance();
   }
 
+  /**
+   * Returns the data as an array.
+   *
+   * @return array The data as an array.
+   */
   #[ArrayShape(['data' => 'mixed', 'skip' => 'int', 'limit' => 'int', 'total' => 'int'])]
   public function toArray(): array
   {
@@ -30,17 +50,32 @@ class ApiResponse
     ];
   }
 
+  /**
+   * Returns the data as JSON.
+   *
+   * @return string The data as JSON.
+   */
   public function toJSON(): string
   {
     return json_encode($this->toArray());
   }
 
+  /**
+   * Returns the data as an array.
+   *
+   * @return array The data as an array.
+   */
   #[ArrayShape(['data' => "mixed", 'skip' => "int", 'limit' => "int", 'total' => "int"])]
   public function __serialize(): array
   {
     return $this->toArray();
   }
 
+  /**
+   * Returns the data as a string.
+   *
+   * @return string The data as a string.
+   */
   public function __toString(): string
   {
     return match(gettype($this->data)) {
@@ -53,6 +88,11 @@ class ApiResponse
     };
   }
 
+  /**
+   * Returns the total number of records.
+   *
+   * @return int The total number of records.
+   */
   private function getTotal(): int
   {
     if (!is_countable($this->data))
