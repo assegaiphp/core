b26fef2edd1f8d1eab7ab3153f8c930182a2a056
Andrew Masiye <amasiye313@gmail.com>
2024-03-04 15:33:32 +0200

    refactor(util): improve DateTime object creation in TypeManager.php
    Updated DateTime object creation to allow dates of multiple formats

--- src/Util/TypeManager.php
+++ src/Util/TypeManager.php
@@ -3,6 +3,9 @@
 namespace Assegai\Core\Util;
 
 use Assegai\Core\Exceptions\Container\EntryNotFoundException;
+use Assegai\Core\Exceptions\Http\HttpException;
+use Assegai\Core\Http\HttpStatus;
+use Assegai\Core\Http\HttpStatusCode;
 use Assegai\Core\Injector;
 use DateTime;
 use ReflectionProperty;
@@ -21,6 +24,7 @@ class TypeManager
    * @param string $targetType The target type to cast to.
    * @return mixed The object cast to the user-defined type.
    * @throws EntryNotFoundException If the target type cannot be found.
+   * @throws HttpException If the object cannot be cast to the target type.
    */
   public static function castObjectToUserType(stdClass $object, string $targetType): mixed
   {
@@ -39,7 +43,14 @@ class TypeManager
         $propertyReflection = new ReflectionProperty($instance, $key);
         if ($propertyReflection->getType()->getName() === 'DateTime')
         {
-          $instance->$key = DateTime::createFromFormat(DATE_ATOM, $value);
+          try
+          {
+            $instance->$key = new DateTime($value);
+          }
+          catch (\Exception $e)
+          {
+            throw new HttpException($e->getMessage(), HttpStatus::BadRequest());
+          }
         }
         else
         {
