ca93971d29b8aa65f65c79872f611f2ff93ef0ad
Andrew Masiye <amasiye313@gmail.com>
2024-06-09 23:45:52 +0200

    fix: fix root module loading

--- src/ControllerManager.php
+++ src/ControllerManager.php
@@ -2,14 +2,25 @@
 
 namespace Assegai\Core;
 
+use Assegai\Core\Attributes\Modules\Module;
 use Assegai\Core\Exceptions\Container\EntryNotFoundException;
 use Assegai\Core\Attributes\Controller;
 use ReflectionAttribute;
 use ReflectionClass;
 use ReflectionException;
+use RuntimeException;
 
+/**
+ * The ControllerManager class is responsible for managing the controllers.
+ *
+ * @package Assegai\Core
+ */
 class ControllerManager
 {
+  protected ModuleManager $moduleManager;
+  /**
+   * @var ControllerManager|null
+   */
   protected static ?ControllerManager $instance = null;
 
   /** @var ReflectionAttribute[] $lastLoadedAttributes */
@@ -18,13 +29,20 @@ class ControllerManager
   /** @var ReflectionClass[] $controllerTokensList */
   protected array $controllerTokensList = [];
 
+  /** @var array $controllerPathTokenIdMap */
   protected array $controllerPathTokenIdMap = [];
 
+  /**
+   * ControllerManager constructor.
+   */
   private final function __construct()
   {
+    $this->moduleManager = ModuleManager::getInstance();
   }
 
   /**
+   * Returns the ControllerManager instance.
+   *
    * @return ControllerManager
    */
   public static function getInstance(): ControllerManager
@@ -38,7 +56,9 @@ class ControllerManager
   }
 
   /**
-   * @return array
+   * Returns the controller token list.
+   *
+   * @return array<string, ReflectionClass>
    */
   public function getControllerTokenList(): array
   {
@@ -46,7 +66,9 @@ class ControllerManager
   }
 
   /**
-   * @return array
+   * Returns the controller path token id map.
+   *
+   * @return array<string, string>
    */
   public function getControllerPathTokenIdMap(): array
   {
@@ -54,8 +76,57 @@ class ControllerManager
   }
 
   /**
-   * @param ReflectionAttribute[] $moduleTokensList
-   * @return array
+   * Returns the root controller class.
+   *
+   * @return string
+   * @throws ReflectionException
+   */
+  public function getRootControllerClass(): string
+  {
+    $rootModuleClass = $this->moduleManager->getRootModuleClass();
+    $rootModuleReflection = new ReflectionClass($rootModuleClass);
+    $attributes= $rootModuleReflection->getAttributes(Module::class);
+
+    if (! $attributes )
+    {
+      throw new RuntimeException('Root module class must be decorated with the Module attribute');
+    }
+
+    /** @var ReflectionAttribute $moduleAttributeReflection */
+    $moduleAttributeReflection = array_pop($attributes);
+
+    $rootControllersClasses = $moduleAttributeReflection->getArguments()['controllers'] ?? [];
+
+    if (empty($rootControllersClasses))
+    {
+      throw new RuntimeException('Root module class must have at least one controller');
+    }
+
+    $rootControllerClass = '';
+
+    foreach ($rootControllersClasses as $index => $controllersClass)
+    {
+      if ($index === 0)
+      {
+        $rootControllerClass = $controllersClass;
+      }
+
+      // Check if the controller has a path === '/'
+      if (isset($this->controllerPathTokenIdMap[$controllersClass]) && $this->controllerPathTokenIdMap[$controllersClass] === '/')
+      {
+        $rootControllerClass = $controllersClass;
+        break;
+      }
+    }
+
+    return $rootControllerClass;
+  }
+
+  /**
+   * Builds the controller tokens list. The controller tokens list is a list of all the controllers in the application.
+   *
+   * @param ReflectionAttribute[] $moduleTokensList The list of module tokens.
+   * @return array<string, ReflectionClass> The controller tokens list.
    * @throws EntryNotFoundException
    */
   public function buildControllerTokensList(array $moduleTokensList): array
@@ -89,7 +160,11 @@ class ControllerManager
   }
 
   /**
-   * @throws EntryNotFoundException
+   * Returns the controller reflection from the given token ID.
+   *
+   * @param string $tokenId The token ID of the controller.
+   * @return ReflectionClass|null The controller reflection.
+   * @throws EntryNotFoundException If the controller is not found.
    */
   private function getControllerReflection(string $tokenId): ?ReflectionClass
   {
