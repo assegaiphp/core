97ab52e78491adf3f6a69782450c79afff5e9d48
Andrew Masiye <amasiye313@gmail.com>
2024-07-02 13:21:54 +0200

    feat(rending): create TemplateEngineInterface.php
    * Created TemplateEngine.php as the base abstract class for all rendering engines
    * Created DefaultTemplateEngine.php
    * Moved ViewEngine.php from src/Rendering to src/Rendering/Engines

---
+++ src/Rendering/Engines/TemplateEngine.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Assegai\Core\Rendering\Engines;
+
+use Assegai\Core\Components\Interfaces\ComponentInterface;
+use Assegai\Core\Enumerations\Http\ContentType;
+use Assegai\Core\Exceptions\RenderingException;
+use Assegai\Core\Rendering\Interfaces\TemplateEngineInterface;
+
+abstract class TemplateEngine implements TemplateEngineInterface
+{
+  /**
+   * The template to render.
+   *
+   * @var string
+   */
+  protected string $template = '';
+  /**
+   * The data to render.
+   *
+   * @var array
+   */
+  protected array $data = [];
+  /**
+   * The content type.
+   *
+   * @var ContentType
+   */
+  protected ContentType $contentType;
+  /**
+   * The root component.
+   *
+   * @var ComponentInterface|null $rootComponent  The root component.
+   */
+  protected ?ComponentInterface $rootComponent = null;
+
+  /**
+   * Constructs a TemplateEngine.
+   */
+  public function __construct()
+  {
+    $this->contentType = ContentType::HTML;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function setTemplate(string $template): TemplateEngineInterface
+  {
+    $this->template = $template;
+    return $this;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function setData(array $data): TemplateEngineInterface
+  {
+    $this->data = $data;
+    return $this;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function setContentType(ContentType $contentType): TemplateEngineInterface
+  {
+    $this->contentType = $contentType;
+    return $this;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function setRootComponent(ComponentInterface $rootComponent): self
+  {
+    $this->rootComponent = $rootComponent;
+
+    $componentAttribute = $this->rootComponent->getAttribute();
+
+    return $this;
+  }
+}
