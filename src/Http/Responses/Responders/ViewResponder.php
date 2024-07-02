0675c4baa83623b481e52157d766f421c0715a75
Andrew Masiye <amasiye313@gmail.com>
2024-07-02 20:36:19 +0200

    feat(http/responses): implement the respond method of the ViewResponder.php

--- src/Http/Responses/Responders/ViewResponder.php
+++ src/Http/Responses/Responders/ViewResponder.php
@@ -2,17 +2,50 @@
 
 namespace Assegai\Core\Http\Responses\Responders;
 
+use Assegai\Core\Enumerations\Http\ContentType;
+use Assegai\Core\Exceptions\Http\InternalServerErrorException;
 use Assegai\Core\Http\HttpStatusCode;
-use Assegai\Core\Http\Responses\Interfaces;
+use Assegai\Core\Http\Responses\Interfaces\ResponderInterface;
+use Assegai\Core\Http\Responses\Response;
+use Assegai\Core\Rendering\Engines\ViewEngine;
+use Assegai\Core\Rendering\View;
 
-class ViewResponder implements Interfaces\ResponderInterface
+/**
+ * ViewResponder class. This class is used by the response object to render a view.
+ *
+ * @package Assegai\Core\Http\Responses\Responders
+ */
+class ViewResponder implements ResponderInterface
 {
+  /**
+   * Constructs a ViewResponder.
+   *
+   * @param ViewEngine $viewEngine The ViewEngine instance.
+   */
+  public function __construct(protected ViewEngine $viewEngine)
+  {
+  }
 
   /**
    * @inheritDoc
    */
   public function respond(mixed $response, int|HttpStatusCode|null $code = null): never
   {
-    // TODO: Implement respond() method.
+    if ($response instanceof Response) {
+      $responseBody = $response->getBody();
+      $response->setContentType(ContentType::HTML);
+
+      if ($responseBody instanceof View) {
+        $this->viewEngine->load($responseBody)->render();
+      }
+
+      throw new InternalServerErrorException("Response body is not a View instance.");
+    }
+
+    if ($response instanceof View) {
+      $this->viewEngine->load($response)->render();
+    }
+
+    throw new InternalServerErrorException("Response is not a View instance.");
   }
 }
