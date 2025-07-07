de60247f917cafbd587896235ee499d11f2fe0f9
Andrew Masiye <amasiye313@gmail.com>
2025-07-07 22:47:52 +0200

    feat: implement queue injection logic

--- src/Queues/Attributes/InjectQueue.php
+++ src/Queues/Attributes/InjectQueue.php
@@ -3,7 +3,9 @@
 namespace Assegai\Core\Queues\Attributes;
 
 use Assegai\Core\Queues\Interfaces\QueueInterface;
+use Assegai\Core\Queues\QueueFactory;
 use Attribute;
+use InvalidArgumentException;
 
 #[Attribute(Attribute::TARGET_PARAMETER)]
 readonly class InjectQueue
@@ -13,9 +15,24 @@ readonly class InjectQueue
   /**
    * InjectQueue constructor.
    *
-   * @param string $name The name of the queue to inject.
+   * @param string $path The path to the queue configuration, e.g., 'redis.default'.
    */
   public function __construct(
-    public string $name = ''
-  ) {}
+    public string $path = ''
+  ) {
+    $queueConfig = config("queues.$path");
+
+    if (empty($queueConfig)) {
+      throw new InvalidArgumentException("Queue configuration for '$path' not found.");
+    }
+
+    [$driver, $name] = explode('.', $path, 2) + [null, null];
+
+    if (is_null($name)) {
+      throw new InvalidArgumentException("Invalid queue path '$path'. Expected format: 'driver.name'.");
+    }
+
+    $queueFactory = QueueFactory::getInstance();
+    $this->queue = $queueFactory->createQueue($driver, $name, $queueConfig);
+  }
 }
