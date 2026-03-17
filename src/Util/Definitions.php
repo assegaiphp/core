d5422ca3b042503adac227084ae0c3a6850575c6
Andrew Masiye <amasiye313@gmail.com>
2026-03-18 00:50:55 +0200

    feat: improve default storage path definition to handle empty or false values

--- src/Util/Definitions.php
+++ src/Util/Definitions.php
@@ -2,7 +2,13 @@
 
 namespace Assegai\Core\Util;
 
-defined('DEFAULT_STORAGE_PATH')     || define('DEFAULT_STORAGE_PATH',     trim(shell_exec('pwd')) . '/uploads');
+$defaultStorageRoot = \getcwd();
+
+if ($defaultStorageRoot === false || $defaultStorageRoot === '') {
+  $defaultStorageRoot = '.';
+}
+
+defined('DEFAULT_STORAGE_PATH')     || define('DEFAULT_STORAGE_PATH',     rtrim($defaultStorageRoot, '/\\') . '/uploads');
 defined('DEFAULT_FIELD_NAME_SIZE')  || define('DEFAULT_FIELD_NAME_SIZE',  100);
 defined('DEFAULT_FIELD_SIZE')       || define('DEFAULT_FIELD_SIZE',       '1MB');
 defined('DEFAULT_MAX_FILE_SIZE')    || define('DEFAULT_MAX_FILE_SIZE',    5000);
